c3f722640bda06726110ba062893b3854eff2f66
ElQwerto110100100 <joshyboy111@protonmail.com>
2023-03-19 14:52:32 +1100

    Implement basic app skeleton

---
+++ build/webpack/webpack.dev.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./client/index.tsx",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "../../public", "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Helsinki City Bike",
+      filename: "../index.html",
+    }),
+  ],
+};
---
+++ client/components/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { FC } from "react";
+
+const App: FC = () => {
+    return <h1>Hello, world</h1>;
+};
+
+export default App;
