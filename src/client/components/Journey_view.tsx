b20466c449dc64550cbe3207c4479e3973baa929
ElQwerto110100100 <joshyboy111@protonmail.com>
2023-03-24 16:13:43 +1100

    add pagination, filtering and sorting to view

--- src/client/components/Journey_view.tsx
+++ src/client/components/Journey_view.tsx
@@ -1,6 +1,8 @@
+import React, { useEffect, useState } from "react"
 import { EuiBasicTableColumn, EuiInMemoryTable } from "@elastic/eui"
 import axios from "axios"
-import React, { useEffect, useState } from "react"
+import moment from "moment"
+import { uniqBy } from "lodash"
 import { Journey_data } from "../../common"
 
 export const Journey_view = () => {
@@ -21,18 +23,8 @@ export const Journey_view = () => {
 
   const columns: EuiBasicTableColumn<Journey_data>[] = [
     {
-      field: "departure_date",
-      name: "Departure date",
-      sortable: true,
-    },
-    {
       field: "departure_station_name",
       name: "Departure station",
-      sortable: true,
-    },
-    {
-      field: "return_date",
-      name: "Return date",
       sortable: true,
     },
     {
@@ -42,12 +34,21 @@ export const Journey_view = () => {
     },
     {
       field: "covered_distance",
-      name: "Covered distance (m)",
+      name: "Covered distance",
+      //Display distance in a more readable formate
+      render: (covered_distance: number) => {
+        if (covered_distance < 1000) {
+          return `${covered_distance} m`
+        } else {
+          return `${(covered_distance / 1000).toFixed(2)} km`
+        }
+      },
       sortable: true,
     },
     {
       field: "duration",
-      name: "Duration (sec.)",
+      name: "Duration",
+      render: (duration: number) => moment.duration(duration, "seconds").humanize(),
       sortable: true,
     },
   ]
@@ -57,6 +58,39 @@ export const Journey_view = () => {
       items={journey_data}
       columns={columns}
       pagination={true}
+      //declaring these props in the component as EUI does not provide types for them.
+      //making it hard to define them outside the component
+      search={{
+        box: {
+          incremental: true,
+        },
+        filters: [
+          {
+            type: "field_value_selection",
+            field: "departure_station_name",
+            name: "Departure station",
+            multiSelect: false,
+            options: uniqBy(journey_data, "departure_station_name").map(
+              (journey_data) => ({
+                value: journey_data.departure_station_name,
+                view: journey_data.departure_station_name,
+              })
+            ),
+          },
+          {
+            type: "field_value_selection",
+            field: "return_station_name",
+            name: "Return station",
+            multiSelect: false,
+            options: uniqBy(journey_data, "return_station_name").map(
+              (journey_data) => ({
+                value: journey_data.return_station_name,
+                view: journey_data.return_station_name,
+              })
+            ),
+          },
+        ],
+      }}
       sorting={{
         sort: {
           field: "id",
