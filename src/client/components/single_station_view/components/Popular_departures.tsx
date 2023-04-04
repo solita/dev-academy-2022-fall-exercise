0d3fc70a5e78323a26ad991dea08381fab8bbf76
ElQwerto110100100 <joshyboy111@protonmail.com>
2023-04-04 17:29:49 +1000

    add missing files

---
+++ src/client/components/single_station_view/components/Popular_departures.tsx
@@ -0,0 +1,42 @@
+import {
+  EuiListGroupItem,
+  EuiTitle,
+  EuiSpacer,
+  EuiListGroup,
+  EuiSkeletonText,
+} from "@elastic/eui"
+import React from "react"
+import { FC } from "react"
+import { Station_stats } from "src/server/controllers/station"
+
+interface Popular_departures_props {
+  station_stats: Station_stats | undefined
+  switch_station: (station_id: string) => void
+}
+const Popular_departures: FC<Popular_departures_props> = ({
+  station_stats,
+  switch_station,
+}) => {
+  const top_departure_stations_list = station_stats?.top_5_departure_stations.map(
+    (station, index) => (
+      <EuiListGroupItem
+        onClick={() => switch_station(station._id)}
+        label={`${index + 1}. ${station.nimi}`}
+      />
+    )
+  )
+
+  return (
+    <>
+      <EuiTitle size="s">
+        <h2>Departing stations</h2>
+      </EuiTitle>
+      <EuiSpacer size="s" />
+      <EuiListGroup flush={true} bordered={true} title="Ending from here">
+        {station_stats ? top_departure_stations_list : <EuiSkeletonText lines={5} />}
+      </EuiListGroup>
+    </>
+  )
+}
+
+export default Popular_departures
---
+++ src/client/components/single_station_view/components/Popular_returns.tsx
@@ -0,0 +1,44 @@
+import {
+  EuiListGroupItem,
+  EuiTitle,
+  EuiSpacer,
+  EuiListGroup,
+  EuiSkeletonText,
+  EuiPanel,
+} from "@elastic/eui"
+import React from "react"
+import { FC } from "react"
+import { Station_stats } from "src/server/controllers/station"
+
+interface Popular_returns_props {
+  station_stats: Station_stats | undefined
+  switch_station: (station_id: string) => void
+}
+
+const Popular_returns: FC<Popular_returns_props> = ({
+  station_stats,
+  switch_station,
+}) => {
+  const top_return_stations_list = station_stats?.top_5_return_stations.map(
+    (station, index) => (
+      <EuiListGroupItem
+        onClick={() => switch_station(station._id)}
+        label={`${index + 1}. ${station.nimi}`}
+      />
+    )
+  )
+
+  return (
+    <EuiPanel>
+      <EuiTitle size="s">
+        <h2>Return stations</h2>
+      </EuiTitle>
+      <EuiSpacer size="s" />
+      <EuiListGroup flush={true} bordered={true}>
+        {station_stats ? top_return_stations_list : <EuiSkeletonText lines={5} />}
+      </EuiListGroup>
+    </EuiPanel>
+  )
+}
+
+export default Popular_returns
